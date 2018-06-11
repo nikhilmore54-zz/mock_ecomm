6c5bf63a364449cee81b304476ab74baafccc720
Nikhil More <nikhilmore54@gmail.com>
2018-06-11 19:42:26 +0530

    Generates image thumbnails

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,10 +1,19 @@
+require 'image_processing/mini_magick'
+
 class ImageUploader < Shrine
+  include ImageProcessing::MiniMagick
+
   plugin :determine_mime_type
   plugin :cached_attachment_data
   plugin :remove_attachment
-  
+  plugin :delete_raw
+
   Attacher.validate do 
     validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
     validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
+  end
+
+  process(:store) do |io, context|
+    { original: io, thumb: resize_to_limit!(io.download, 300, 300) }
   end
 end

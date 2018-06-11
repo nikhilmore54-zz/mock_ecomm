80a99b399ac369cfe64e36452bdf71801c3ac398
Nikhil More <nikhilmore54@gmail.com>
2018-06-11 19:03:03 +0530

    Validates image upload

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -1,2 +1,9 @@
 class ImageUploader < Shrine
+  plugin :determine_mime_type
+  plugin :cached_attachment_data
+  
+  Attacher.validate do 
+    validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
+    validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
+  end
 end

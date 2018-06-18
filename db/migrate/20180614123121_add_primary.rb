53005aa109aa6c537e3d9be95087fef56127afe3
Nikhil More <nikhilmore54@gmail.com>
2018-06-18 17:14:57 +0530

    create cart table changed migration file 3

--- db/migrate/20180614123121_add_primary.rb
+++ db/migrate/20180614123121_add_primary.rb
@@ -2,7 +2,7 @@ class AddPrimary < ActiveRecord::Migration[5.2]
   def change
     # execute "ALTER TABLE carts ADD PRIMARY KEY (id);"
     # execute "ALTER TABLE line_items ADD PRIMARY KEY (id);"
-    execute "ALTER TABLE photos ADD PRIMARY KEY (id);"
+    # execute "ALTER TABLE photos ADD PRIMARY KEY (id);"
     execute "ALTER TABLE orders ADD PRIMARY KEY (id);"
   end
 end
